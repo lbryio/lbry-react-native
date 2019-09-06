1dc85cc4c9eb632c3ac8466605b256aa182f3783
Akinwale Ariwodola <akinwale@gmail.com>
2019-09-06 19:41:15 +0100

    fix featured search result item stuck loading

--- src/component/fileListItem/view.js
+++ src/component/fileListItem/view.js
@@ -13,6 +13,10 @@ import ProgressBar from 'component/progressBar';
 import fileListStyle from 'styles/fileList';
 
 class FileListItem extends React.PureComponent {
+  state = {
+    url: null,
+  };
+
   getStorageForFileInfo = fileInfo => {
     if (!fileInfo.completed) {
       const written = formatBytes(fileInfo.written_bytes);
@@ -44,8 +48,8 @@ class FileListItem extends React.PureComponent {
 
   componentDidUpdate() {
     const { claim, resolveUri, uri } = this.props;
-    if (!claim) {
-      resolveUri(uri);
+    if (!claim && uri !== this.state.url) {
+      this.setState({ url: uri }, () => resolveUri(uri));
     }
   }
 
