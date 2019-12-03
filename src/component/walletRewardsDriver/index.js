fa768c1c592c22dda2b9af215e760fe2efe4d5d9
Akinwale Ariwodola <akinwale@gmail.com>
2019-12-03 15:14:16 +0100

    update text for signed in users

--- src/component/walletRewardsDriver/index.js
+++ src/component/walletRewardsDriver/index.js
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
-import { selectUnclaimedRewardValue } from 'lbryinc';
+import { selectUnclaimedRewardValue, selectUser } from 'lbryinc';
 import WalletRewardsDriver from './view';
 
 const select = state => ({
   unclaimedRewardAmount: selectUnclaimedRewardValue(state),
+  user: selectUser(state),
 });
 
 export default connect(select)(WalletRewardsDriver);
--- src/component/walletRewardsDriver/view.js
+++ src/component/walletRewardsDriver/view.js
@@ -5,15 +5,25 @@ import walletStyle from 'styles/wallet';
 
 class WalletRewardsDriver extends React.PureComponent<Props> {
   render() {
-    const { navigation, unclaimedRewardAmount } = this.props;
+    const { navigation, unclaimedRewardAmount, user } = this.props;
+    const signedIn = user && user.has_verified_email;
 
     return (
       <TouchableOpacity style={walletStyle.rewardDriverCard} onPress={() => navigation.navigate('Rewards')}>
         <Icon name="award" size={16} style={walletStyle.rewardIcon} />
-        <Text style={walletStyle.rewardDriverText}>
-          Get {unclaimedRewardAmount > 0 ? unclaimedRewardAmount : ''} free credit
-          {unclaimedRewardAmount === 1 ? '' : 's'} after creating an account.
-        </Text>
+        {signedIn && (
+          <Text style={walletStyle.rewardDriverText}>
+            {unclaimedRewardAmount > 0 ? unclaimedRewardAmount : ''} free credit{unclaimedRewardAmount === 1 ? '' : 's'}{' '}
+            available in rewards. Tap to learn more.
+          </Text>
+        )}
+
+        {!signedIn && (
+          <Text style={walletStyle.rewardDriverText}>
+            Get {unclaimedRewardAmount > 0 ? unclaimedRewardAmount : ''} free credit
+            {unclaimedRewardAmount === 1 ? '' : 's'} after creating an account.
+          </Text>
+        )}
       </TouchableOpacity>
     );
   }
