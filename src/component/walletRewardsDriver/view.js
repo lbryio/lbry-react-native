3237652d35a600124d62f6e159909447c9aa4899
Akinwale Ariwodola <akinwale@gmail.com>
2019-12-06 16:01:25 +0100

    update text for signed in users (#89)
    * update text for signed in users
    * i18n tweaks

--- src/component/walletRewardsDriver/view.js
+++ src/component/walletRewardsDriver/view.js
@@ -5,15 +5,31 @@ import walletStyle from 'styles/wallet';
 
 class WalletRewardsDriver extends React.PureComponent<Props> {
   render() {
-    const { navigation, unclaimedRewardAmount } = this.props;
+    const { navigation, unclaimedRewardAmount, user } = this.props;
+    const signedIn = user && user.has_verified_email;
 
     return (
       <TouchableOpacity style={walletStyle.rewardDriverCard} onPress={() => navigation.navigate('Rewards')}>
         <Icon name="award" size={16} style={walletStyle.rewardIcon} />
-        <Text style={walletStyle.rewardDriverText}>
-          Get {unclaimedRewardAmount > 0 ? unclaimedRewardAmount : ''} free credit
-          {unclaimedRewardAmount === 1 ? '' : 's'} after creating an account.
-        </Text>
+        {signedIn && (
+          <Text style={walletStyle.rewardDriverText}>
+            {unclaimedRewardAmount === 0 && __('Free credits available in rewards.')}
+            {unclaimedRewardAmount === 1 &&
+              __('%amount% free credit available in rewards.', { amount: unclaimedRewardAmount })}
+            {unclaimedRewardAmount > 1 &&
+              __('%amount% free credits available in rewards.', { amount: unclaimedRewardAmount })}{' '}
+            {__('Tap to learn more.')}
+          </Text>
+        )}
+
+        {!signedIn && (
+          <Text style={walletStyle.rewardDriverText}>
+            {unclaimedRewardAmount === 1 &&
+              __('Get %amount% free credit after creating an account.', { amount: unclaimedRewardAmount })}
+            {unclaimedRewardAmount !== 1 &&
+              __('Get %amount% free credits after creating an account.', { amount: unclaimedRewardAmount })}
+          </Text>
+        )}
       </TouchableOpacity>
     );
   }
