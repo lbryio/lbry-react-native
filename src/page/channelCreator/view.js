4e6267d5a93f62fe66e9a225562d194338041405
Akinwale Ariwodola <akinwale@gmail.com>
2020-01-23 00:55:45 +0100

    additional thumbnailUrl check

--- src/page/channelCreator/view.js
+++ src/page/channelCreator/view.js
@@ -931,7 +931,7 @@ export default class ChannelCreator extends React.PureComponent {
                       source={{ uri: thumbnailUrl }}
                     />
                   )}
-                  {(thumbnailUrl === null || thumbnailUrl.trim().length === 0) && newChannelName.length > 0 && (
+                  {(!!thumbnailUrl || thumbnailUrl.trim().length === 0) && newChannelName.length > 0 && (
                     <Text style={channelIconStyle.autothumbCharacter}>
                       {newChannelName.substring(0, 1).toUpperCase()}
                     </Text>
