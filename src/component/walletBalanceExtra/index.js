aa5dd878c0638128db2c6f48472ead47e75b0bfa
Akinwale Ariwodola <akinwale@gmail.com>
2020-03-31 21:17:21 +0100

    fix displayed wallet custody message when signed in

--- src/component/walletBalanceExtra/index.js
+++ src/component/walletBalanceExtra/index.js
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux';
 import { selectClaimsBalance, selectSupportsBalance, selectTipsBalance } from 'lbry-redux';
+import { makeSelectClientSetting } from 'redux/selectors/settings';
 import WalletBalanceExtra from './view';
+import Constants from 'constants'; // eslint-disable-line node/no-deprecated-api
 
 const select = state => ({
   claimsBalance: selectClaimsBalance(state) || 0,
+  deviceWalletSynced: makeSelectClientSetting(Constants.SETTING_DEVICE_WALLET_SYNCED)(state),
   supportsBalance: selectSupportsBalance(state) || 0,
   tipsBalance: selectTipsBalance(state) || 0,
 });
 
-export default connect(
-  select,
-  null,
-)(WalletBalanceExtra);
+export default connect(select, null)(WalletBalanceExtra);
